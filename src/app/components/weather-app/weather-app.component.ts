9afd9425492ddf26c556829234722d4ca231ef05
tony2tones <anthonymarques@outlook.com>
2021-11-11 13:59:01 +0200

    added geolocation functions

--- src/app/components/weather-app/weather-app.component.ts
+++ src/app/components/weather-app/weather-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Forecast } from 'src/app/models/forecast.models';
 import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
@@ -8,13 +9,50 @@ import { WeatherService } from 'src/app/services/weather.service';
 })
 export class WeatherAppComponent implements OnInit {
 
+  public forecast: Forecast | undefined;
+  public errormsg: string = '';
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    if(!navigator.geolocation) {
-      
+    // for browsers that do not have geolcation support
+    if (!navigator.geolocation) {
+      this.errormsg = 'geolocation not supportd'
     }
+    this.getForecast();
+  }
+
+
+  getForecast() {
+    this.resetForecast();
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition(this.success, this.error, options);
+  }
+  public success(pos: any) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+  public error(err: any) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+
+  // this.weatherService.getWeather().subscribe((data) => {
+  //   console.log('this is the weather',data);
+  // })
+
+  resetForecast() {
+    this.forecast = undefined;
   }
 
 }
